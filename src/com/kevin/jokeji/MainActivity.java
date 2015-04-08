84626962e8e73cecb29f6c4a7113e3d6c39f3397
kevin chen <chenyi_de_email@163.com>
2015-04-08 15:49:06 +0800

    update theme

--- src/com/kevin/jokeji/MainActivity.java
+++ src/com/kevin/jokeji/MainActivity.java
@@ -7,8 +7,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 
@@ -55,25 +53,6 @@ public class MainActivity extends ActionBarActivity implements
 		mTextFragment = new TextFragment();
 		mManager.beginTransaction().add(R.id.content, mNewFragment).commit();
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
